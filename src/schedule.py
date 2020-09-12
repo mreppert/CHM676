33d753ba8972444b45d1164404b64adb75650ae0
mreppert <reppertm@control.nanohub.org>
2020-09-12 10:07:48 -0400

    updated video links

--- src/schedule.py
+++ src/schedule.py
@@ -138,12 +138,12 @@ def build_schedule_f2020(srcdir):
     schd.add_content(content('compute', 'Python Crash Course: Python Arrays', link=srcdir+'programming/arrays.ipynb', vlink='https://983291-6.kaf.kaltura.com/media/t/1_arhakfmo/177251882'))
     schd.add_content(content('compute', 'Python Crash Course: Molecular Dynamics', link=srcdir+'programming/md.ipynb'))
 
-    schd.add_content(content('lecture', 'Maxwell\'s equations and the Lorentz Force Law', link='https://mreppert.github.io/education/chm676f20/notes/MaxwellsEquations.pdf'))
-    schd.add_content(content('lecture', 'Electromagnetic Waves in Vacuum', link='https://mreppert.github.io/education/chm676f20/notes/VacuumWaves.pdf'))
-    schd.add_content(content('compute', 'Fourier Transforms', link=srcdir+'FT/FourierTransforms.ipynb'))
+    schd.add_content(content('lecture', 'Maxwell\'s equations and the Lorentz Force Law', link='https://mreppert.github.io/education/chm676f20/notes/MaxwellsEquations.pdf', vlink='https://purdue.brightspace.com/d2l/home/52483'))
+    schd.add_content(content('lecture', 'Electromagnetic Waves in Vacuum', link='https://mreppert.github.io/education/chm676f20/notes/VacuumWaves.pdf', vlink='https://purdue.brightspace.com/d2l/home/52483'))
+    schd.add_content(content('compute', 'Fourier Transforms', link=srcdir+'FT/FourierTransforms.ipynb'), vlink='https://purdue.brightspace.com/d2l/home/52483')
 
-    schd.add_content(content('lecture', 'Energy Content in EM Waves', link='https://mreppert.github.io/education/chm676f20/notes/EnergyContent.pdf'))
-    schd.add_content(content('lecture', 'Microscopic Electrodynamics: The Wave Equation', link='https://mreppert.github.io/education/chm676f20/notes/MicroscopicElectrodynamics.pdf'))
+    schd.add_content(content('lecture', 'Energy Content in EM Waves', link='https://mreppert.github.io/education/chm676f20/notes/EnergyContent.pdf', vlink='https://purdue.brightspace.com/d2l/home/52483'))
+    schd.add_content(content('lecture', 'Microscopic Electrodynamics: The Wave Equation', link='https://mreppert.github.io/education/chm676f20/notes/MicroscopicElectrodynamics.pdf', vlink='https://purdue.brightspace.com/d2l/home/52483'))
     schd.add_content(content('compute', 'MD in an Electric Field', link=srcdir+'MDinEField/MD.ipynb'))
 
     schd.add_content(content('lecture', 'Macroscopic Electrodynamics: Ensemble-Averaged Fields'))
