4db8091ee23dfafa88ad0e1768707659f0516028
mreppert <reppertm@control.nanohub.org>
2020-09-30 17:34:37 -0400

    updated video for Maxwells Equations lecture

--- src/schedule.py
+++ src/schedule.py
@@ -138,7 +138,7 @@ def build_schedule_f2020(srcdir):
     schd.add_content(content('compute', 'Python Crash Course: Python Arrays', link=srcdir+'programming/arrays.ipynb', vlink='https://983291-6.kaf.kaltura.com/media/t/1_arhakfmo/177251882'))
     schd.add_content(content('compute', 'Python Crash Course: Molecular Dynamics', link=srcdir+'programming/md.ipynb'))
 
-    schd.add_content(content('lecture', 'Maxwell\'s equations and the Lorentz Force Law', link='https://mreppert.github.io/education/chm676f20/notes/MaxwellsEquations.pdf', vlink='https://purdue.brightspace.com/d2l/home/52483'))
+    schd.add_content(content('lecture', 'Maxwell\'s equations and the Lorentz Force Law', link='https://mreppert.github.io/education/chm676f20/notes/MaxwellsEquations.pdf', vlink='https://youtu.be/bwC38gprBo4'))
     schd.add_content(content('lecture', 'Electromagnetic Waves in Vacuum', link='https://mreppert.github.io/education/chm676f20/notes/VacuumWaves.pdf', vlink='https://purdue.brightspace.com/d2l/home/52483'))
     schd.add_content(content('compute', 'Fourier Transforms', link=srcdir+'FT/FourierTransforms.ipynb', vlink='https://purdue.brightspace.com/d2l/home/52483'))
 
